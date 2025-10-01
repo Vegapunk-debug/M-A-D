f7e9cd485ad685fff6782adc3e7db9ac250a9b0b
Rohit Nair P <rohitnairmuttathethu@gmail.com>
2025-10-01 11:02:12 +0530

    Added data of app users

---
+++ learn_loop/src/data/RandomData.js
@@ -0,0 +1,44 @@
+export const data = [
+    {
+        id: 1, 
+        name: "Tony Stark", 
+        skills: ["Python", "Natural Language Processing", "Operating System Design"],
+        wantsToLearn: ["Quantum Mechanics", "Biotechnology"],
+    },
+    {
+        id: 2, 
+        name: "Steve Rogers", 
+        skills: ["Algorithm Design", "Data Structures", "Cybersecurity"],
+        wantsToLearn: ["Python", "Robotics", "AI"],
+    },   
+    {
+        id: 3, 
+        name: "Thor Odinson", 
+        skills: ["JavaScript", "Os"],
+        wantsToLearn: ["Quantum Mechanics", "Python", "AI"],
+    },    
+    {
+        id: 4, 
+        name: "Bruce Banner", 
+        skills: ["Cloud Computing", "Parallel Processing"],
+        wantsToLearn: ["Mobile App Development", "Database Management"],
+    },    
+    {
+        id: 5, 
+        name: "Natasha Romanoff", 
+        skills: ["Mobile App Development", "Database Management", "Web Development"],
+        wantsToLearn: ["Cloud Computing", "Parallel Processing"],
+    },    
+    {
+        id: 6, 
+        name: "Peter Parker", 
+        skills: ["React", "React Native"],
+        wantsToLearn: ["AI", "Python"],
+    },    
+    {
+        id: 7, 
+        name: "Thanos", 
+        skills: ["Algorithm", "Data Mining"],
+        wantsToLearn: ["Pandas, Javascript"],
+    },
+] 
