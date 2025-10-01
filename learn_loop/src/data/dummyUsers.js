1d7421d99b44935348978c0e857db1aac1f6a1ae
Rohit Nair P <rohitnairmuttathethu@gmail.com>
2025-10-01 11:14:47 +0530

    Added users data

--- learn_loop/src/data/dummyUsers.js
+++ learn_loop/src/data/dummyUsers.js
@@ -1,27 +1,27 @@
-export const users = [
-    {
-      id: 1,
-      name: "Tony Stark",
-      canTeach: ["Coding", "DSA"],
-      wantsToLearn: ["Cooking", "Guitar"]
-    },
-    {
-      id: 2,
-      name: "Steve",
-      canTeach: ["Coding", "Development"],
-      wantsToLearn: ["Cooking", "Photography"]
-    },
-    {
-      id: 3,
-      name: "The Rock",
-      canTeach: ["Rustling"],
-      wantsToLearn: ["Cooking", "Coding"]
-    },
-    {
-        id: 4,
-        name: "Vin Diesel",
-        canTeach: ["Driving"],
-        wantsToLearn: ["Singing", "Coding"]
-      }
-  ]
+// export const users = [
+//     {
+//       id: 1,
+//       name: "Tony Stark",
+//       canTeach: ["Coding", "DSA"],
+//       wantsToLearn: ["Cooking", "Guitar"]
+//     },
+//     {
+//       id: 2,
+//       name: "Steve",
+//       canTeach: ["Coding", "Development"],
+//       wantsToLearn: ["Cooking", "Photography"]
+//     },
+//     {
+//       id: 3,
+//       name: "The Rock",
+//       canTeach: ["Rustling"],
+//       wantsToLearn: ["Cooking", "Coding"]
+//     },
+//     {
+//         id: 4,
+//         name: "Vin Diesel",
+//         canTeach: ["Driving"],
+//         wantsToLearn: ["Singing", "Coding"]
+//       }
+//   ]
   
